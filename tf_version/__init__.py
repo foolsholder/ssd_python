b7e8b80d3683c1539c21b3f7b638d4a9010d01d5
theherowho <myarctan@gmail.com>
2021-08-28 21:56:49 +0300

    __init__

--- tf_version/__init__.py
+++ tf_version/__init__.py
@@ -1,3 +1,5 @@
 from .ssd_v2 import SSD300v2
 from .ssd_utils import BBoxUtility
-__all__ = ['SSD300v2', 'BBoxUtility']
+from .ssd_layers import Normalize, PriorBox
+
+__all__ = ['SSD300v2', 'BBoxUtility', 'Normalize', 'PriorBox']
--- torch_version/__init__.py
+++ torch_version/__init__.py
@@ -1,4 +1,5 @@
 from .ssd_v2 import SSD300v2
 from .ssd_utils import BBoxUtility
+from .ssd_layers import Normalize, PriorBox
 
-__all__ = ['SSD300v2', 'BBoxUtility']
+__all__ = ['SSD300v2', 'BBoxUtility', 'Normalize', 'PriorBox']
